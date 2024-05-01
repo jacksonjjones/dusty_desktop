1c79541d60083ef2794087fe04e5517c98ae66ad
jacksonjjones <jacksonjjones1@gmail.com>
2024-05-01 17:44:03 -0500

    completed register Route

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,30 +1,48 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// Importing necessary modules from Workbox
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Precache all the assets defined in the service worker manifest file
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Define a CacheFirst strategy for caching pages with specified options
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
+    // Cache responses with status codes 0 and 200
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+    // Set expiration for cached responses to 30 days
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
+// Warm the cache for specified URLs using the CacheFirst strategy
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Register a route for navigation requests using the pageCache strategy
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Register a route for caching assets with a StaleWhileRevalidate strategy
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      // Cache responses with status codes 0 and 200
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
